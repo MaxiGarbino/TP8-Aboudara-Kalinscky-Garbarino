96d7050ce4d21864dee8421a141d9072d74f57f1
MAXIMILIANO GARBARINO <47433817@ort>
2023-09-12 08:12:48 -0300

    fads

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TP8_Aboudara_Kalinscky_Garbarino.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
